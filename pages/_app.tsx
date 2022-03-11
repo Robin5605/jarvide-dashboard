d14be36bc8267455338a9da02776d2a6fdc3ceb8
Robin5605 <robinjefferson123@gmail.com>
2022-03-10 21:45:05 -0600

    Custom App (meta head tags)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta property='og:title' content='Welcome to Jarvide' />
+        <meta property='og:site_name' content='Jarvide' />
+        <meta property='og:description' content='A simple and easy IDE-liked Discord bot.' />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp
